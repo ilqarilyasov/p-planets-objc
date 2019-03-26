a9feed5cb8d7b501916e3255e889c40b9ecacf91
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-03-25 21:33:37 -0500

    Initial Commit

---
+++ P-planets-objc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  P-planets-objc
+//
+//  Created by Ilgar Ilyasov on 3/25/19.
+//  Copyright © 2019 Fun-the-mental. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
